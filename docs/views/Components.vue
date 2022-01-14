7507e8e365a0cf1277449c0b6adef3777c3c73a1
Swaana <swaanas@gmail.com>
2022-01-14 13:52:16 +0100

    Added copy webpack plugin, added Github logos and using logo with link in header now. (#7)
    Co-authored-by: Swaana Snorradottir <Swaana.Snorradottir@ictu.nl>

--- docs/views/Components.vue
+++ docs/views/Components.vue
@@ -1,9 +1,6 @@
 <template>
   <ul>
     <li>
-      <router-link to="/">Home</router-link>
-    </li>
-    <li>
       <router-link to="/components/grid">Grid</router-link>
     </li>
   </ul>
--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -2,16 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const { VueLoaderPlugin } = require('vue-loader')
 
 const isProd = process.env.NODE_ENV === 'production'
+const dest = path.resolve(__dirname, 'dist')
 
 module.exports = {
   mode: isProd ? 'production' : 'development',
   entry: './docs/main.js',
   output: {
     publicPath: isProd ? '/sass/' : '',
-    path: path.resolve(__dirname, 'dist'),
+    path: dest,
     filename: 'bundle.js',
   },
   resolve: {
@@ -37,11 +39,14 @@ module.exports = {
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({ template: './docs/index.html' }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new CopyPlugin({
+      patterns: [{ from: './docs/static', to: path.join(dest, 'static') }]
+    })
   ],
   devServer: {
     hot: true,
     static: {
-      directory: path.join(__dirname, 'docs/dist'),
+      directory: dest,
     },
   }
 };
