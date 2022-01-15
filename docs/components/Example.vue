50d9b2620f043cf28c54944a53de7a5c9da371c1
Swaana Snorradottir <Swaana.Snorradottir@ictu.nl>
2022-01-15 11:53:52 +0100

    Enabled primitive code examples

--- docs/components/Example.vue
+++ docs/components/Example.vue
@@ -1,21 +1,51 @@
 <template>
-  <div class="firedew-example">
+  <div class="docs-example" ref="exampleRef">
     <slot/>
+  </div>
+  <div class="docs-code">
+    <pre><code>{{ source }}</code></pre>
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
+
 export default {
-  name: 'Example'
+  name: 'Example',
+  props: {
+    source: { type: String, default: '' }
+  },
+  setup (props) {
+    const exampleRef = ref(null);
+    onMounted(() => {
+      if(props.source) {
+        exampleRef.value.innerHTML = props.source;
+      }
+    })
+
+    return {
+      exampleRef
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import './lib/colors';
 
-.firedew-example {
+.docs-example {
   border: 1px solid $gray-200;
   padding: 16px;
-  border-radius: 4px;
+  border-radius: 4px 4px 0 0;
+}
+.docs-code {
+  background-color: $gray-100;
+  border: 1px solid $gray-200;
+  padding: 16px;
+  border-radius: 0 0 4px 4px;
+  margin-bottom: 16px;
+}
+pre {
+  overflow-x: auto;
 }
 </style>
